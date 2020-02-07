19786fd7b59c5dc914c6f6fbf7edeaf66ba1faf9
chalatz <chralatz@gmail.com>
2020-02-07 14:17:26 +0200

    Working on menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,14 +3,17 @@
     // Toggles the sliding of the mobile menu
     var handle_menu = function() {
 
-        var menu_header = $('#site-nav__header')
-            site_nav = $('#site-nav');
+        var menu_burger = $('#site-nav__burger')
+            site_nav = $('#site-nav'),
+            site_menu_items = $('#site-nav__items');
 
-        menu_header.on('click', function(e){
-            site_menu.toggleClass('site-menu--mobile');
+        menu_burger.on('click', function(e){
+            site_nav.toggleClass('site-nav--mobile');
             site_menu_items.slideToggle();
         });
 
     };
+
+    handle_menu();
 
 })(jQuery);
