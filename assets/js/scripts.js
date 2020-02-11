ff6da48c418d48339fd5a26afb70a820d59974a9
chalatz <chralatz@gmail.com>
2020-02-11 14:09:12 +0200

    Worked on mobile submenu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,11 +9,35 @@
 
         menu_burger.on('click', function(e){
             site_nav.toggleClass('site-nav--mobile');
+            site_menu_items.toggleClass('site-nav__items--opened');
             site_menu_items.slideToggle();
         });
 
     };
 
+    // Toggles the sliding of the mobilemenu's subitems
+    var handle_submenu = function() {
+    
+        var parent_item = $('.site-nav__item-link--with-sublinks');
+
+        parent_item.on('click', function(e){
+            e.preventDefault();                
+            var $this = $(this),
+                submenu = $this.siblings('.site-nav__subitems'),
+                chevron_icon = $this.siblings('.site-nav__chevron').find('.chevron-icon');
+
+            $this.children().on('click', function(e){
+                e.stopPropagation();
+            });
+            
+            chevron_icon.toggleClass('rotate-180');
+            
+            submenu.slideToggle();
+        });
+
+    }
+
     handle_menu();
+    handle_submenu();
 
 })(jQuery);
