179effd057d3c05c7d30a4182c204dec73cd8f4e
chalatz <chralatz@gmail.com>
2020-11-03 10:42:19 +0200

    Added nocaptcha

--- sendmail-mobile-nocaptcha.php
+++ sendmail-mobile-nocaptcha.php
@@ -2,7 +2,7 @@
 
 $debug = false;
 
-if ($debug) {
+if ($debug == true) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -15,11 +15,16 @@ $infodb_api_key = include '_infodb_key.php';
 $ipinfodb = new IPInfoDB($infodb_api_key);
 $results = $ipinfodb->getCity($_SERVER['REMOTE_ADDR']);
 
+$apots = include '_apots.php';
+
 $return_to = '';
 $from_page = '';
 $first_last_name = '';
 $e_mail = '';
 $comments = '';
+$the_page = '';
+$apot = '';
+
 $ipAddress = $results['ipAddress'];
 $countryCode = $results['countryCode'];
 $countryName = $results['countryName'];
@@ -42,6 +47,18 @@ if(isset($_POST['e_mail'])){
 }
 if(isset( $_POST['comments'])){
     $comments = $_POST['comments'];
+}
+if(isset( $_POST['the_page'])){
+    $the_page = $_POST['the_page'];
+}
+if(isset( $_POST['apot'])){
+    $apot = trim($_POST['apot']);
+}
+
+if(array_key_exists($the_page, $apots)) {
+    $the_apot = $apots[$the_page];
+} else {
+    $the_apot = 5;
 }
 
 function passed(){
@@ -79,23 +96,6 @@ $msg .= "City Name: $cityName\r\n";
 $msg .= "Zip Code: $zipCode\r\n";
 $msg .= "Time Zone: $timeZone\r\n";
 
-if ($debug) {
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-    if (validated()) {
-        if (passed()) {
-            echo 'passed!\n';
-        } else {
-            echo 'failed!\n';
-        }
-    } else {
-        echo 'validation error\n';
-    }
-    print_r($msg);
-    die();
-}
-
 $headers = "Content-Type: text/html; charset=UTF-8";
 
 // include_once('_keys.php');
@@ -110,49 +110,75 @@ function validated() {
     if(isset($_POST['comments']) && $_POST['comments'] == ''){
         return false;
     }
+    if(isset($_POST['apot']) && $_POST['apot'] == ''){
+        return false;
+    }
 
     return true;
 }
 
-function passed_recaptcha(){
+function passed_nocaptcha($apot, $the_apot){
 
     if (passed()) {
-        $url = 'https://www.google.com/recaptcha/api/siteverify';
-
-        $key = include '_recaptcha_key.php';
-
-        $response = file_get_contents($url."?secret=".$key."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']);
-
-        $data = json_decode($response);
-
-        if(isset($data->success) && $data->success == true){
+        if ($apot == $the_apot) {
             return true;
         } else {
             return false;
         }
-
     }
-
-    // if (passed()) {
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-
 }
 
-if (validated()) {
-    if(passed_recaptcha() && mail($address, $e_subject, $msg, "From: $e_mail\r\nReply-To: $e_mail\r\nReturn-Path: $e_mail\r\nContent-Type: text/plain; charset=UTF-8\r\n")){
-        // Email has sent successfully, echo a success page.
+// if ($debug == true) {
+//     echo "<pre>";
+//     print_r($_POST);
+//     echo "</pre>";
+//     echo "<br>";
+//     echo "the apot: " . $the_apot;
+//     echo "<br>";
+//     if (passed_nocaptcha($apot, $the_apot)) {
+//         echo '<br> Passed no captcha!!<br>';
+//     }
+//     if (validated()) {
+//         if (passed()) {
+//             echo 'passed!\n';
+//         } else {
+//             echo 'failed!\n';
+//         }
+//     } else {
+//         echo 'validation error\n';
+//     }
+//     print_r($msg);
+//     die();
+// }
 
-        header('Location: ' . $return_to . '?contact-form-sent=success');
+if ($debug == true) {
+    if (validated()) {
+        if(passed_nocaptcha($apot, $the_apot)){
+            // Email has sent successfully, echo a success page.
 
+            header('Location: ' . $return_to . '?contact-form-sent=success');
+
+        } else {
+            header('Location: '. $return_to . '?contact-form-sent=fail-mobile');
+        }
     } else {
-        header('Location: '. $return_to . '?contact-form-sent=fail-mobile');
+        header('Location: '. $return_to . '?contact-form-sent=validation-error');
     }
 } else {
-    header('Location: '. $return_to . '?contact-form-sent=validation-error');
+    if (validated()) {
+        if(passed_nocaptcha($apot, $the_apot) && mail($address, $e_subject, $msg, "From: $e_mail\r\nReply-To: $e_mail\r\nReturn-Path: $e_mail\r\nContent-Type: text/plain; charset=UTF-8\r\n")){
+            // Email has sent successfully, echo a success page.
+
+            header('Location: ' . $return_to . '?contact-form-sent=success');
+
+        } else {
+            header('Location: '. $return_to . '?contact-form-sent=fail-mobile');
+        }
+    } else {
+        header('Location: '. $return_to . '?contact-form-sent=validation-error');
+    }
 }
+
 
 
 ?>
